7e628f99f89c4f6975107a5f4a425270ba72b604
Alberto Cabello Sánchez <alberto@unex.es>
2021-06-11 10:11:57 +0200

    Improve HTTPS check

--- Extra.py
+++ Extra.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python3
 
+import datetime
+import dateutil.parser
 import http.client
 import threading
+import pytz
+import urllib.request
 import wx
 
 import Resources
@@ -9,7 +13,7 @@ import Resources
 class HTTPServers(Resources.ThreadedPanel):
 
     def __init__(self, parent=None):
-        Resources.ThreadedPanel.__init__(self, parent, ['Servidor', 'Puerto', 'Status'])
+        Resources.ThreadedPanel.__init__(self, parent, ['Server', 'Port', 'Status', 'Certificate Expiry Date'])
 
 
 class GetHTTPThread(threading.Thread):
@@ -23,6 +27,7 @@ class GetHTTPThread(threading.Thread):
         for HTTPServer in self.servers:
             HTTPServerName, HTTPServerPort = HTTPServer.split(":")
             status = "unknown"
+            expiryDate = ""
 
             try:
                 connection = http.client.HTTPSConnection(HTTPServerName, HTTPServerPort, timeout=2)
@@ -31,8 +36,9 @@ class GetHTTPThread(threading.Thread):
                 Resources.debug_print("HTTPS {0} gives {1}.".format(HTTPServer, response.status))
                 if response.status != None:
                     status = "running"
+                    expiryDate = checkX509ExpiryDate(HTTPServerName, HTTPServerPort)
             except ConnectionRefusedError:
-                record = [HTTPServerName, HTTPServerPort, "stopped"]
+                record = [HTTPServerName, HTTPServerPort, "stopped", expiryDate]
             except Exception as ex:
                 Resources.debug_print("HTTPS {0} gives error {1}.".format(HTTPServer, str(ex)))
             
@@ -48,5 +54,15 @@ class GetHTTPThread(threading.Thread):
             except Exception as ex:
                 Resources.debug_print("HTTP {0} gives error {1}.".format(HTTPServer, str(ex)))
 
-            record = [HTTPServerName, HTTPServerPort, status]
+            record = [HTTPServerName, HTTPServerPort, status, expiryDate]
             wx.CallAfter(self.control.UpdateRow, record)
+
+def checkX509ExpiryDate(ServerName, Port='443'):
+    context = urllib.request.ssl.create_default_context()
+    with urllib.request.socket.create_connection((ServerName, Port)) as sock:
+        ssock = context.wrap_socket(sock, server_hostname=ServerName)
+    notAfter = ssock.getpeercert()['notAfter']
+    date = dateutil.parser.parse(notAfter)
+    now = datetime.datetime.now()
+    remaining = (date - pytz.timezone('Europe/Madrid').localize(now)).days
+    return "{0} (expires in {1} days)".format(notAfter, remaining)
